a75286a196c9a860472fea3412e21c815513eee1
MuslumYilmaz <muslum@vizyon.ai>
2019-05-17 16:11:30 +0300

    final commit

--- index.js
+++ index.js
@@ -1,65 +1,21 @@
-const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos');
-const erase = document.querySelector('.delete');
-const search = document.querySelector('.search input');
+const clock = document.querySelector('.clock');
 
-//Generate the new todo template
-const generateTemplate = todo => {
+const tick = () => {
+
+    const now = new Date();
+
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+  
     const html = `
-            <li class="added list-group-item d-flex justify-content-between align-items-center">
-            <span>${todo}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>
-    `
-    list.innerHTML += html;
-   
-};
+        <span>${h}</span> :
+        <span>${m}</span> :
+        <span>${s}</span>
+    `;
 
+    clock.innerHTML = html;
 
-// Add Todo
-addForm.addEventListener('submit', e => {
+}
 
-    e.preventDefault();
-    const todo = addForm.add.value.trim();
-    if (todo != ''){
-        generateTemplate(todo);
-        addForm.reset();
-    } else {
-        //do nothing
-    }
-})
-
-
-
-
-
-
-// Delete Todo
-list.addEventListener('click', e => {
-    
-    if (e.target.classList.contains('delete')){
-        e.target.parentElement.remove();
-    }
-});
-
-//Search Todo
-
-const filterTodos = (term) => {
-
-    Array.from(list.children)
-    .filter(todo =>  !todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.add('filtered'));  
-
-
-    Array.from(list.children)
-    .filter(todo =>  todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.remove('filtered'));  
-  }  
-//keyup event
-search.addEventListener('keyup', () => {  
-   const term = search.value.trim().toLowerCase();
-   filterTodos(term);
-  });
-
-
- 
+setInterval(tick,1000);
